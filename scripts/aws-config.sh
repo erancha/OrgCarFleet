75396838378361e9a29577dbbc703c4efd362ae3
Eran Hachmon <57663148+erancha@users.noreply.github.com>
2025-12-05 14:56:19 +0200

    Delete scripts/aws-config.sh

--- scripts/aws-config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Set temporary AWS credentials for current shell session
-export AWS_ACCESS_KEY_ID="AKIAUO3N43FANVPK7VWE"
-export AWS_SECRET_ACCESS_KEY="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"
-export AWS_DEFAULT_REGION="eu-central-1"
-
-# echo "AWS credentials set for this session"
